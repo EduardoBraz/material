a3ab61642547491f495cec22bc1e02abd0614a26
Eduardo Vale <edubvale@gmail.com>
2016-08-24 17:15:03 -0300

    Adding custom class to the container
    Adding custom class to the container

--- src/components/dialog/dialog.js
+++ src/components/dialog/dialog.js
@@ -658,7 +658,14 @@ function MdDialogProvider($$interimElementProvider) {
         // an element outside of the container, and the focus trap won't work probably..
         // Also the tabindex is needed for the `escapeToClose` functionality, because
         // the keyDown event can't be triggered when the focus is outside of the container.
-        return '<div class="md-dialog-container" tabindex="-1">' + validatedTemplate(template) + '</div>';
+        var containerClass = "md-dialog-container";
+        
+        if (options.containerClass) {
+          containerClass += " " + options.containerClass;
+        }
+
+        return '<div class="' + containerClass + '" tabindex="-1">' + validatedTemplate(template) + '</div>';
+
 
         /**
          * The specified template should contain a <md-dialog> wrapper element....
